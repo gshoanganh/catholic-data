352cab67472544e96c8a0990b6c129711a09bc05
gshoanganh <lequochoang@live.com>
2024-03-13 16:55:10 +0700

    up

--- catholic1.js
+++
@@ -1,8 +0,0 @@
-const info = {
-version: 1,
-author: "gshoanganh",
-first: "2024-02-26",
-last: "2024-02-26"
-}
-const colors = ["#000"]
-const url = "https://drive.google.com/file/d/1kEzsSCmpmV-cJPGsXE0eEKWTAgaPlJ8h/view?usp=sharing"
